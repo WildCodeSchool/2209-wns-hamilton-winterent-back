ca0d454258e3514752cf29ec997d5dc561259806
davy lebris <davylebris@gmail.com>
2023-06-21 11:14:18 +0200

    test login

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -8,7 +8,7 @@ import fetch from 'cross-fetch';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://localhost:8000/graphql',
+    uri: 'http://api:8000/graphql',
     fetch,
   }),
   cache: new InMemoryCache({}),
